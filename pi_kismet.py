26b7d1d3a584645829129243701545fea67eea58
Sean Beck <seanmckaybeck@gmail.com>
2014-03-31 11:48:46 -0600

    Finished main routine. Added doc strings
    Main routine is in pi_kismet.py and creates everything
    
    Doc strings added to pi_kismet_display.py

--- pi_kismet.py
+++ pi_kismet.py
@@ -1,1 +1,52 @@
-import argparse
+import argparse, sys, time
+sys.path.append("/home/pi/Adafruit-Raspberry-Pi-Python-Code/Adafruit_CharLCDPlate")
+from Adafruit_CharLCDPlate import Adafruit_CharLCDPlate
+from pi_kismet_display import KismetDisplay
+
+HOLD_TIME = 3.0
+REFRESH_TIME = 5.0
+display = KismetDisplay()
+lcd = display.lcd
+prevCol = -1
+prev = -1
+lastTime = time.time()
+
+parser = argparse.ArgumentParser(prog='KismetDisplay', version='1.0')
+parser.add_argument('-f', '--filename', type=str, help='The XML log file from Kismet')
+args = parser.parse_args()
+if not args.filename:
+    print 'Error: no file specified'
+    exit(1)
+
+display.init_info(args.filename)
+
+def shutdown():
+    lcd.clear()
+    exit(0)
+
+# Listen for button presses
+while True:
+    b = lcd.buttons()
+    if b is not prev:
+        if lcd.buttonPressed(lcd.SELECT):
+            tt = time.time() # Start time of button press
+            while lcd.buttonPressed(lcd.SELECT): # Wait for button release
+                if (time.time() - tt) >= HOLD_TIME: # Extended hold?
+                    shutdown() # We're outta here
+            display.backlightStep()
+        elif lcd.buttonPressed(lcd.LEFT):
+                display.scrollRight()
+        elif lcd.buttonPressed(lcd.RIGHT):
+                display.scrollLeft()
+        elif lcd.buttonPressed(lcd.UP):
+                display.modeUp()
+        elif lcd.buttonPressed(lcd.DOWN):
+                display.modeDown()
+        prev = b
+        lastTime = time.time()
+    else:
+        now = time.time()
+        since = now - lastTime
+        if since > REFRESH_TIME or since < 0.0:
+            display.update()
+            lastTime = now
--- pi_kismet_display.py
+++ pi_kismet_display.py
@@ -3,7 +3,7 @@ from pi_kismet_info import KismetInfo
 
 class KismetDisplay:
     lcd = Adafruit_CharLCDPlate()
-    mode = 1
+    mode = 0
     screen = []
     col = []
     prevCol = 0
@@ -20,6 +20,11 @@ class KismetDisplay:
         self.lcd.backlight(self.col[self.prevCol])
 
     def display_screen(self, new_screen):
+        """
+        A general function for displaying a specified screen
+
+        new_screen is a list with two elements to be displayed
+        """
         self.screen = new_screen
         try:
             self.maxOffset = max((len(self.screen[0]) - 16), (len(self.screen[1]) - 16))
@@ -32,60 +37,92 @@ class KismetDisplay:
 
 
     def init_info(self, filename):
+        """
+        Initializes the KismetInfo member variable
+
+        filename is the log file from Kismet that will be parsed for data
+        """
         self.info = KismetInfo(filename)
         self.display_total()
 
     def display_total(self):
+        """
+        Displays the total number of networks discovered
+        """
         self.display_screen(self.info.screen1)
 
     def display_wpa(self):
+        """
+        Displays the total number of networks using WPA
+        """
         self.display_screen(self.info.screen2)
 
     def display_wep(self):
+        """
+        Displays the total number of networks using WEP
+        """
         self.display_screen(self.info.screen3)
 
     def display_none(self):
+        """
+        Displays the total number of networks with no encryption
+        """
         self.display_screen(self.info.screen4)
 
     def display_wps(self):
+        """
+        Displays the total number of networks using WPS
+        """
         self.display_screen(self.info.screen5)
         
     # the code below this line is taken directly from PiMinerDisplay
     # except for the changes made to update()
 
-    #Cycle Backlight Color / On/Off
     def backlightStep(self):
+        """
+        Cycles the backlight color on/off
+        """
         if self.prevCol is (len(self.col) -1): self.prevCol = -1
         newCol = self.prevCol + 1
         self.lcd.backlight(self.col[newCol])
         self.prevCol = newCol
     
-    #Offset text to the right
     def scrollLeft(self):
+        """
+        Offsets the text to the right
+        """
         if self.offset >= self.maxOffset: return
         self.lcd.scrollDisplayLeft()
         self.offset += 1
     
-    #Offset text to the left
     def scrollRight(self):
+        """
+        Offsets the text to the left
+        """
         if self.offset <= 0: return
         self.lcd.scrollDisplayRight()
         self.offset -= 1
     
-    #Display next info screen
     def modeUp(self):
+        """
+        Displays the next info screen on up button push
+        """
         self.mode += 1
         if self.mode > 4: self.mode = 0
         self.update()
     
-    #Display previous info screen
     def modeDown(self):
+        """
+        Displays the prior info screen on down button push
+        """
         self.mode -= 1
         if self.mode < 0: self.mode = 4
         self.update()
     
-    #Update display
     def update(self):
+        """
+        Updates the display based on the mode
+        """
         self.info.refresh()
         if self.mode == 0: self.display_total()
         elif self.mode == 1: self.display_wpa()
