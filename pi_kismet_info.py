bcf38fa259669a26fc6628756881dadc18a4cd9f
Sean Beck <seanmckaybeck@gmail.com>
2014-03-31 13:35:19 -0600

    Info now correctly counts WPA v WEP v None
    Also was missing call to parse_xml() in refresh() function of
    KismetInfo

--- pi_kismet_info.py
+++ pi_kismet_info.py
@@ -25,21 +25,21 @@ class KismetInfo:
         self.doc = etree.parse(self.filename)
         self.root = doc.getroot()
         self.total_networks = len(root) - 1 # all children are networks except first one which is card info
-        nodes = doc.findall('wireless-network/SSID/encryption') #TODO how to ignore multiple children?
-        # for i in range(self.last_index, len(
+        nodes = doc.xpath('//wireless-network/SSID/encryption[1]/text()')
         none = wep = wpa = wps = 0
         # TODO add in checking for WPS. need to do test run with kismet development version
-        for node in nodes:
-            if 'None' in node.text:
+        for text in nodes:
+            if 'None' in text:
                 none += 1
-            elif 'WPA' in node.text:
+            elif 'WPA' in text:
                 wpa += 1
-            elif 'WEP' in node.text:
+            elif 'WEP' in text:
                 wep += 1
         self.total_wep = wep
         self.total_wpa = wpa
         self.total_none = none
     def refresh(self):
+        self.parse_xml()
         self.screen1[0] = 'Total Networks: '
         self.screen1[1] = '%d' % self.total_networks
         self.screen2[0] = 'Total WPA: '
